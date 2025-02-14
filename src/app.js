45162a3c7b222075fb08662535ecd98132ed3d85
Pedro Arruda <arruda.pedro15@outlook.com>
2025-02-13 21:21:10 -0300

    refactor: standardize import statements and remove commented-out code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import {customerRoutes} from './routes/customer-routes.js'
-import { dishRoutes } from './routes/dish-routes.js';
-import { orderRoutes } from './routes/order-routes.js';
-import setupSwagger from './infra/swagger.js';
-import sequelize from './infra/database/config/index.js';
+import express from "express";
+import { customerRoutes } from "./routes/customer-routes.js";
+import { dishRoutes } from "./routes/dish-routes.js";
+import { orderRoutes } from "./routes/order-routes.js";
+import setupSwagger from "./infra/swagger.js";
+import errorHandler from "./middlewares/error-handler.js";
 
 export const app = express();
 
@@ -11,22 +11,6 @@ app.use(express.json());
 
 setupSwagger(app);
 
-// const env = process.env.NODE_ENV || 'development';
-// const dbConfig = config()[env];
-
-// const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((error) => {
-    console.error('Unable to connect to the database:', error);
-  });
-
-
-
-app.use('/customer', customerRoutes);
-app.use('/', dishRoutes);
-app.use('/', orderRoutes);
-
+app.use("/customer", customerRoutes);
+app.use("/", dishRoutes);
+app.use("/", orderRoutes);
